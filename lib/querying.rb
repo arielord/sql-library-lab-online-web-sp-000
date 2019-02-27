58e5db657c7c98e81a270e35e717ddf955554672
Ariel <arielord@buffalo.edu>
2019-02-27 12:56:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,13 +6,12 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
-
 def select_value_and_count_of_most_prolific_species
   "SELECT species, count(species) FROM characters GROUP BY species ORDER BY count(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT series.name, series.subgenre"
+  "SELECT authors.name, subgenre.title"
 end
 
 def select_series_title_with_most_human_characters
